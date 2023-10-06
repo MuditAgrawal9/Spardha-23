0ba9b2d28b7334b570c9ea92618ffb128b6d5ae2
anshuman-nivas <125132858+anshuman-nivas@users.noreply.github.com>
2023-10-06 23:45:55 +0530

    [ADDED] document creation

--- frontend/src/components/DashBoard/Document/Document.js
+++ frontend/src/components/DashBoard/Document/Document.js
@@ -1,7 +1,29 @@
+import React, { useState } from 'react';
 import './Document.css';
-import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios';
 
 const Document = () => {
+  const [inputValue, setInputValue] = useState(''); // State to store the input value
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleFormSubmit = async(e) => {
+    e.preventDefault();
+    const token = localStorage.getItem('token');
+    console.log('Input Value:', inputValue);
+    const response=await axios.post(process.env.REACT_APP_BASE_URL+"documents/",{
+      document:JSON.parse({
+        aadhar:inputValue
+      })
+    },{
+      headers:{
+        Authorization:"Token "+token
+      }
+    })
+    console.log(response)
+  };
 
   return (
     <div className="user-dashboard3 profile_Pad">
@@ -9,12 +31,17 @@ const Document = () => {
         <div>
           <div className="doc_upper_content">
             <div className="mb-3">
-            <br /><br/>
+              <br /><br />
               <label htmlFor="formFile" className="form-label">Upload Your Document For Verification</label>
-              {/* <input className="form-control" type="file" id="formFile" /> */}
-              <input type="text" class="form-control" placeholder="Input" />
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Input"
+                value={inputValue}
+                onChange={handleInputChange}
+              />
             </div>
-            <button type="button" className="btn btn-outline-success">Upload</button>
+            <button type="button" className="btn btn-outline-success" onClick={handleFormSubmit}>Upload</button>
           </div>
           <div className="doc_lower_content">
             <div className="status">
@@ -22,9 +49,9 @@ const Document = () => {
               <div className="col"><h5>Verified</h5></div>
             </div>
           </div>
-          <div class="form-floating">
-            <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2"></textarea>
-            <label for="floatingTextarea2">Comments</label>
+          <div className="form-floating">
+            <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea2"></textarea>
+            <label htmlFor="floatingTextarea2">Comments</label>
           </div>
         </div>
       </div>
